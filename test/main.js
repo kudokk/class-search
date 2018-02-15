da47dae4cacf5663b6215beb52f246feab96ec28
KUDO Kazuki <kudo-k@gnavi.co.jp>
2018-02-15 18:16:40 +0900

    2018_0215_1816

--- test/main.js
+++ test/main.js
@@ -1,24 +1,31 @@
 const fs = require('fs');
+const dirPath = 'test/expect/';
+let textObj = {};
 
+// ファイルネームを投げたらbufferが取れる
 const rf = (fileName) => {
-  const readableStream = fs.createReadStream('test/expect/' + fileName);
+  const readableStream = fs.createReadStream(dirPath + fileName);
   readableStream.on('data', function(data) {
-    console.log(data);
-    console.log(data.toString());
     return data.toString();
   });
 };
 
-fs.readdir('test/expect', (err, data) => {
+// dataの書き出し
+const wf = (data) => {
+  fs.appendFile(dirPath + 'message.txt', data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log('The file has been saved!');
+  });
+}
+
+// ディレクトリの読み込み。ディレクトリ内のファイル取り出し
+fs.readdir(dirPath, (err, data) => {
   if (err) {
     console.log(err);
   }
   console.log(data);
-  const text = data.map(rf);
-  console.log(text);
+  textObj.ab = data.map(rf);
+  console.log(textObj);
 });
-
-// fs.open('test1.txt', 'r', (err, fd) => {
-//   if (err) console.log(err);;
-//   console.log(fd);
-// })
